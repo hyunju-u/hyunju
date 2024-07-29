7fae886569c0ab68501f66684f11e2de7612f98b
hyunju <hjyu@asiae.co.kr>
2024-07-29 23:42:49 +0900

    init

---
+++ very_first_gpt_api.py
@@ -0,0 +1,17 @@
+import openai
+def ask_to_qpt_35_turbo(user_input):
+    response=openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        top_p=0.1,
+        temperature=0.1,
+        messages=[
+            {"role":"system", "content": "You are the mirror of Snow white."},
+            {"role":"user","content":user_input}
+        ]
+    )
+    return response.choices[0].message.content
+users_request='''
+거울아 거울아 세상에서 누가 제일 예쁘니
+'''
+r=ask_to_qpt_35_turbo(users_request)
+print(r)
